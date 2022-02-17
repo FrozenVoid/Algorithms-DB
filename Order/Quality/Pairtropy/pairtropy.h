4cade864a2e6abeef43e4f43d383b1866fd6c9b8
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-02-17 19:02:36 +0200

    Update pairtropy.h

--- Order/Quality/Pairtropy/pairtropy.h
+++ Order/Quality/Pairtropy/pairtropy.h
@@ -5,8 +5,11 @@ invcube[0]=2.0;
 for(size_t i=1;i<65536;i++){
 f64 r=1.0/(i);invcube[i]=r*r*r;
 }}
-//return  pair density 
+//return  pair density
  u16 dev[65536]={0};f64 lenm=1.0;
-for(size_t i=0;i<len-1;i++)dev[(data[i]<<8)+data[i+1]]++;
+for(size_t i=0;i<len-1;i++){
+u16 devents=dev[(data[i]<<8)+data[i+1]];
+dev[(data[i]<<8)+data[i+1]]=devents!=0xffff?devents+1:devents;
+;}
 for(size_t i=0;i<65536;i++)lenm+=invcube[dev[i]];
 return (65536.0/lenm);}
