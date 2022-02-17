10c21d3f63b1b73caabe97cb763778fd1f857856
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-02-17 21:41:33 +0200

    Update pairtropy.h

--- Order/Quality/Pairtropy/pairtropy.h
+++ Order/Quality/Pairtropy/pairtropy.h
@@ -16,22 +16,19 @@ return (65536.0/lenm);}
 
 
 f64 tripletropy(u8* data,size_t len){
-static f64 invsq[65536];static int sqset=0;
+static f64 invsq[256];static int sqset=0;
 if(!sqset){sqset=1;invsq[0]=1.0;
-for(size_t i=1;i<65536;i++){
+for(size_t i=1;i<256;i++){
 f64 r=1.0/(i+1);invsq[i]=r*r;}}
 //return  triple density
-
-static u32 dev[256][256][256];
+static u8 dev[256][256][256];
  f64 lenm=0.0;
 for(size_t i=0;i<len-2;i++){
-dev[data[i]][data[i+1]][data[i+2]]++;}
+u8 c=++dev[data[i]][data[i+1]][data[i+2]];
+dev[data[i]][data[i+1]][data[i+2]]-=c==0;}
 
 for(size_t i=0;i<256;i++){
 for(size_t k=0;k<256;k++){
 for(size_t z=0;z<256;z++){
-size_t devt=dev[i][k][z]<65536?dev[i][k][z]:65535;lenm+=invsq[devt];
-dev[i][k][z]=0;
-}}}
-
+;lenm+=invsq[dev[i][k][z]];dev[i][k][z]=0;}}}
 return ((256.0*256.0*256.0)-(lenm))/(256.0*256.0*256.0);}
