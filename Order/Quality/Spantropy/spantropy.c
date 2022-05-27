9a1463a1b77e89f84da977091207f1e59ccfb03c
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-05-27 08:25:46 +0300

    Update spantropy.c

--- Order/Quality/Spantropy/spantropy.c
+++ Order/Quality/Spantropy/spantropy.c
@@ -5,9 +5,9 @@ for(size_t i=0;i<65535;i++){first[i]=0;last[i]=0;}
 u16* vdata;
 for(size_t i=0;i<len-1;i++){
 vdata=(u16*)&data[i];
-const size_t curfirst=first[vdata[i]];
-first[vdata[i]]=curfirst?:i+1;
-last[vdata[i]]=i+1;}
+const size_t curfirst=first[*vdata];
+first[*vdata]=curfirst?:i+1;
+last[*vdata]=i+1;}
 size_t occ=0,sumdist=0;
 for(size_t i=0;i<65535;i++){
 if(!first[i])continue;
